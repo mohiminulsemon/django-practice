4a31bd4acb2e3a89147a8f99f9582e4ddfcbe359
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-12-02 22:25:21 +0600

    added 10.5 practice

--- module_10.5/simple_navigation/simple_navigation/urls.py
+++ module_10.5/simple_navigation/simple_navigation/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_template_filters project.
+URL configuration for simple_navigation project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
