daaf51ddb8772ea3a6bef084d0bcbb894f1a50d7
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-01-25 02:20:54 +0600

    django DRF practice

---
+++ rest framework/DRF_project/Accounts/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from . import models
+from django.contrib.auth.models import User
+
+class RegistrationSerializer(serializers.ModelSerializer):
+
+    confirm_password = serializers.CharField(required = True)
+
+    class Meta:
+        model = User
+        fields = ['username','first_name', 'last_name','email', 'password', 'confirm_password']
+
+    def save(self):
+        username = self.validated_data['username']
+        first_name = self.validated_data['first_name']
+        last_name = self.validated_data['last_name']
+        email = self.validated_data['email']
+        password = self.validated_data['password']
+        password2 = self.validated_data['confirm_password']
+
+        if password != password2:
+            raise serializers.ValidationError({'error': "Passwords doesn't matched"})
+        
+        if User.objects.filter(email = email).exists():
+            raise serializers.ValidationError({'error': "Email already exists"})
+        
+        account = User(email = email, username = username, first_name = first_name, last_name = last_name)
+        account.set_password(password)
+        account.is_active = False 
+        account.save()
+        return account
+    
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required = True)
+    password = serializers.CharField(required = True)
