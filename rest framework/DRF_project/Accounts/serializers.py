8ea1372b8084d90c43695ed3754f631c71630234
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-01-27 01:38:00 +0600

    updated models

--- rest framework/DRF_project/Accounts/serializers.py
+++ rest framework/DRF_project/Accounts/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
-from . import models
+from .models import UserDetails
 from django.contrib.auth.models import User
+GENDER_TYPE = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+)
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
-    confirm_password = serializers.CharField(required = True)
+    birth_date = serializers.DateField(write_only=True, input_formats=['%Y-%m-%d'], required=False)
+    gender = serializers.ChoiceField(choices=GENDER_TYPE)
+    profile_pic = serializers.ImageField(required=False)
+    confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ['username','first_name', 'last_name','email', 'password', 'confirm_password']
+        fields = ['username','first_name', 'last_name','email', 'password', 'confirm_password', 'birth_date', 'gender', 'profile_pic']
 
     def save(self):
         username = self.validated_data['username']
@@ -17,6 +25,10 @@ class RegistrationSerializer(serializers.ModelSerializer):
         email = self.validated_data['email']
         password = self.validated_data['password']
         password2 = self.validated_data['confirm_password']
+        # profile_pic = self.validated_data['profile_pic']
+        birth_date = self.validated_data['birth_date']
+        gender = self.validated_data['gender']
+        profile_pic = self.validated_data.get('profile_pic', None)
 
         if password != password2:
             raise serializers.ValidationError({'error': "Passwords doesn't matched"})
@@ -28,6 +40,9 @@ class RegistrationSerializer(serializers.ModelSerializer):
         account.set_password(password)
         account.is_active = False 
         account.save()
+
+        UserDetails.objects.create(user = account, profile_pic = profile_pic, birth_date = birth_date, gender = gender)
+
         return account
     
 
--- rest framework/DRF_project/Accounts/views.py
+++ rest framework/DRF_project/Accounts/views.py
@@ -56,7 +56,7 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('register')
+        return redirect('login')
     else:
         return redirect('register')
     
